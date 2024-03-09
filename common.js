f6a9f29c79babc6f5c99242ccc11b635f8f4032e
Muhammad Faiz <mfcc64@gmail.com>
2024-03-09 08:39:57 +0700

    common.js: use showcqt@2

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 
 async function start_showcqtbar(width, height, bar_h, stream) {
-    var {ShowCQT} = await import("https://cdn.jsdelivr.net/npm/showcqt@1/showcqt.mjs");
+    var {ShowCQT} = await import("https://cdn.jsdelivr.net/npm/showcqt@2/showcqt.mjs");
     var audio_ctx = new(window.AudioContext || window.webkitAudioContext)();
     function resume_audio_ctx() {
         if (audio_ctx.state === "suspended") {
